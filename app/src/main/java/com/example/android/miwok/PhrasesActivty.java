4472854d62a76faafc37b51e7e23006fca9566f8
joel-g <joelaguerra1@gmail.com>
2018-01-21 20:32:01 -0800

    convert numbers xml to be a universal word liost

--- app/src/main/java/com/example/android/miwok/PhrasesActivty.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivty.java
@@ -11,7 +11,7 @@ public class PhrasesActivty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
 
