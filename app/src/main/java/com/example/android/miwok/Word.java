5214212dad19a23e4b2e54c6fb55378527150586
joel-g <joelaguerra1@gmail.com>
2018-01-23 23:19:10 -0800

    adds images to all objects

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,26 +10,33 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
-
+    private int mImageResourceId;
 
     public Word (String defaultTranslation, String miwokTranslation) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
     }
 
+    public Word (String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
-
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
+    public int getImageResourceId() { return mImageResourceId; }
 
     public void setMiwok(String text) {
         mMiwokTranslation = text;
     }
-
     public void setDefaultTranslation(String text) {
         mDefaultTranslation = text;
     }
+    public void setImageResourceId(int id) { mImageResourceId = id; }
+
 }
